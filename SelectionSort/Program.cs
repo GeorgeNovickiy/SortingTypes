96836a470178b8b5a7dd73659443d12c348eb794
George <madeinerussia@yandex.ru>
2023-02-12 16:10:55 +0500

    Добавлены два вспомогательных модуля

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,22 +1,8 @@
-﻿void SortSelection(int[] collection)
-{
-    int size = collection.Length;
-    for (int i =0; i < size -1; i++)
-    {
-        int posMin = i;
-        for (int j = i+1; j < size; j++)
-        {
-            if (collection[j]<collection[posMin]) posMin = j;
-        }
-        int temp = collection [i];
-        collection [i] = collection[posMin];
-        collection[posMin] = temp;
-    }
+﻿using static Sorting;
+using static Infrastructure;
 
-}
 
-int[] array = {7, 6, 3, 4, 5, 1, 2, 3};
-
-Console.WriteLine($"[{String.Join(',', array)}]");
+int[] array = CreateArray(10);
+Show(array);
 SortSelection(array);
-Console.WriteLine($"[{String.Join(',', array)}]");
+Show(array);
---
+++ SelectionSort/Sorting.cs
@@ -0,0 +1,19 @@
+public static class Sorting
+{
+    public static void SortSelection(int[] collection)
+{
+    int size = collection.Length;
+    for (int i =0; i < size -1; i++)
+    {
+        int posMin = i;
+        for (int j = i+1; j < size; j++)
+        {
+            if (collection[j]<collection[posMin]) posMin = j;
+        }
+        int temp = collection [i];
+        collection [i] = collection[posMin];
+        collection[posMin] = temp;
+    }
+
+}
+}
